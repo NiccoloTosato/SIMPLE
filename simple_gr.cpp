7ed423f49099bb27dc613478be202b77bd3c7acc
NiccoloTosato <niccolo.tosato@yahoo.it>
2022-03-15 21:01:52 +0100

    finalmente qualcosa di decente

--- simple_gr.cpp
+++ simple_gr.cpp
@@ -1,83 +1,122 @@
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cmath>
+#include <utility>
+#include <memory>
+
+template <typename T=double,typename index_type=unsigned int>
+
+class Grid {
+private:
+
+  std::unique_ptr<T[]> data;
+  index_type nrows;
+  index_type ncols;
+  
+public:
+  Grid(const std::size_t rows,const std::size_t cols) : data{new T[ rows* cols]},  nrows{ rows }, ncols{ cols} {}
+
+  T& operator()(index_type row, index_type col) {
+    return data[row*ncols + col];
+  }
+
+};
+
 int main() {
 
-  const unsigned int nx = 64;
-  const unsigned int ny = 64;
+  const unsigned int nx {64};
+  const unsigned int ny {64};
 
-  const double lx = 1;
-  const double ly = 1;
+  const double lx {1};
+  const double ly {1};
+  
+  const double dx { lx / nx };
+  const double dy { ly / ny };
+  const double dx2 { dx * dx };
+  const double dy2 { dy * dy };
+  
+  const double i_dx { 1 / dx };
+  const double i_dy { 1 / dy };
+  const double i_dx2 { 1 / dx2 };
+  const double i_dy2 { 1 / dy2 };
+  
+  const double dt { 0.00005 };
+  const double pr { 1 };
+  const double  gr { 10 };
+  const double i_gr { 1 / gr };
+  const double rel { 1 };
+  
+  const double eps_p { 1.0E-10 };
+  const double eps { 1.0E-10 };
+  
 
-  const double dx = lx / nx;
-  const double dy = ly / ny;
-  const double dx2 = dx * dx;
-  const double dy2 = dy * dy;
+  Grid<double,unsigned short int> vx{ny+1,nx};
+  Grid<double,unsigned short int> vx1{ny+1,nx};
+  
+  Grid<double,unsigned short int> vy{ny,nx+1};
+  Grid<double,unsigned short int> vy1{ny,nx+1};
+ 
+  Grid<double,unsigned short int> p{ny+1,nx+1};
+  Grid<double,unsigned short int> p1{ny+1,nx+1};
+  Grid<double,unsigned short int> p2{ny+1,nx+1};
 
-  const double i_dx = 1 / dx;
-  const double i_dy = 1 / dy;
-  const double i_dx2 = 1 / dx2;
-  const double i_dy2 = 1 / dy2;
-
-  const double dt = 0.00005;
-  const double pr = 1;
-  const double gr = 10;
-  const double i_gr = 1 / gr;
-  const double rel = 1;
-
-  const double eps_p = 1.0E-10;
-  const double eps = 1.0E-10;
-
-  double *vx = calloc((ny + 1) * (nx), sizeof(double));
-  double *vx1 = calloc((ny + 1) * (nx), sizeof(double));
-
-  double *vy = calloc((ny) * (nx + 1), sizeof(double));
-  double *vy1 = calloc((ny) * (nx + 1), sizeof(double));
-
-  double *p = calloc((ny + 1) * (nx + 1), sizeof(double));
-  double *p1 = calloc((ny + 1) * (nx + 1), sizeof(double));
-  double *p2 = calloc((ny + 1) * (nx + 1), sizeof(double));
-
-  double *div = calloc((ny) * (nx), sizeof(double));
-
-  double *T = calloc((ny + 1) * (nx + 1), sizeof(double));
-  double *T1 = calloc((ny + 1) * (nx + 1), sizeof(double));
-  while (1) {
-    // x direction
-    for (unsigned int j = 1; j < ny; ++j)
-      for (unsigned int i = 1; i < nx - 1; ++i) {
-
-        double vt =
-            0.5 * (vy[j - 1 + i * (nx + 1)] + vy[j - 1 + (i + 1) * (nx + 1)]);
-        double vb = 0.5 * (vy[j + i * (nx + 1)] + vy[j + (i + 1) * (nx + 1)]);
-
-        double ut = 0.5 * (vx[j - 1 + i * nx] + vx[j + i * nx]);
-        double ub = 0.5 * (vx[j + 1 + i * nx] + vx[j + i * nx]);
-        double ur = 0.5 * (vx[j + i * nx] + vx[j + (i + 1) * nx]);
-        double ul = 0.5 * (vx[j + i * nx] + vx[j + (i - 1)] * nx);
-
+  Grid<double,unsigned short int> div{ny,nx};
+  
+  Grid<double,unsigned short int> T{ny+1,nx+1};
+  Grid<double,unsigned short int> T1{ny+1,nx+1};
+  while(true) {
+    //componente x velocità
+    for( std::size_t j=1; j < ny; ++j) //new row
+      for( std::size_t i=1; i < nx-1; ++i) { //next col
+	const double vt=0.5*(vy(j-1,i)+vy(j-1,i+1));
+	const double vb=0.5*(vy(j,i)+vy(j,i+1));
+	const double ut=0.5*(vx(j-1,i)+vx(j,i));
+	const double ub=0.5*(vx(j+1,i)+vx(j,i));
+	const double ur=0.5*(vx(j,i)+vx(j,i+1));
+	const double ul=0.5*(vx(j,i)+vx(j,i-1));
+  
+	const double a=-(ur*ur-ul*ul)*i_dx-(ub*vb-ut*vt)*i_dy; //convettivo
+	const double b=(vx(j,i+1)-2*vx(j,i)+vx(j,i-1))*i_dx2; //diffusivo 1	// !b=(vy(j,i+1)-2*vy(j,i)+vy(j,i)-1)*i_dx2; !----ERRORE--------ERRORE--------ERRORE--------ERRORE--------ERRORE----
+	const double c=(vx(j+1,i)-2*vx(j,i)+vx(j-1,i))*i_dy2; //diffusivo 2
+	const double AA=-a+sqrt(i_gr)*(b+c);
+	vx1(j,i)=vx(j,i)+dt*(AA-(p(j,i+1)-p(j,i))*i_dx); //velocita vx al passo n+1    
       }
     
-    // y direction
+    //componente y velocità
+    for( std::size_t j=1; j < ny - 1; ++j) //new row
+      for( std::size_t i=1; i < nx; ++i) { //next col
+	const double ur=0.5*(vx(j,i)+vx(j+1,i));
+	const double ul=0.5*(vx(j,i-1)+vx(j+1,i-1));
+	const double vr=0.5*(vy(j,i+1)+vy(j,i));
+	const double vl=0.5*(vy(j,i-1)+vy(j,i));
+	const double vt=0.5*(vy(j-1,i)+vy(j,i));
+	const double vb=0.5*(vy(j+1,i)+vy(j,i));
+	const double a=-(vb*vb-vt*vt)*i_dy-(vr*ur-vl*ul)*i_dx; //convettivo
+	const double b=(vy(j,i+1)-2*vy(j,i)+vy(j,i-1))*i_dx2; //diffusivo 1
+	const double c=(vy(j+1,i)-2*vy(j,i)+vy(j-1,i))*i_dy2; //diffusivo 2
+	const double temp=-(T1(j+1,i)+T1(j,i))*0.5; //termine galleggiamento
+	const double BB=-a-temp+sqrt(i_gr)*(b+c); //approssimo gr>>1 gr/re^2
+	vy1(j,i)=vy(j,i)+dt*(BB-(p(j+1,i)-p(j,i))*i_dy); //velocita vy al passo n+1
+    }
 
-    for (unsigned int j = 1; j < ny - 1; ++j)
-      for (unsigned int i = 1; i < nx; ++i) {
+    //condizioni al contorno vx
+    //impermeabilità
+    for(std::size_t i=1; i<ny; ++i) {
+      vx1(2:ny+1,1)=0;
+      vx1(2:ny+1,nx+1)=0;
+    }
+    
+    //parete top e bottom noslip
+    vx1(1,1:nx+1)=2*0-vx1(2,1:nx+1);
+    vx1(ny+2,1:nx+1)=-vx1(ny+1,1:nx+1);
 
-        double ur = 0.5 * (vx[j + i * nx] + vx[j + 1 + i * nx]);
-        double ul = 0.5 * (vx[j + (i - 1) * nx] + vx[j + 1 + (i-1) * nx]);
-        double vr = 0.5 * (vy[j + i*(nx+1) + 1] + vy[j+ i*(nx+1)]);
-	double vl =0.5 * (vy[j + (i - 1)*(nx+1)] + vy[j + i*(nx+1)]) ;
-	double vt =0.5 * (vy[j - 1 + i*(nx+1)] + vy[j + i*(nx+1)]);
-	double vb =   0.5 * (vy[j + 1 + i*(nx+1)] + vy[j + i*(nx+1)]);
-	
-	double a=-(vb*vb-vt*vt)*i_dy-(vr*ur-vl*ul)*i_dx; //convettivo
-	double b=(vy[j + (i+1) * (nx+1)]-2*vy[j + i * (nx+1)]+vy[ j + (i-1) * (nx+1) ])*i_dx2; //diffusivo 1
-	double c=(vy[j+1+i*(nx+1)]-2*vy[j + i*(nx+1)]+vy[j-1+i*(nx+1)])*i_dy2; //diffusivo 2
-	double temp=-(T1[j+1+i*(nx+1)]+T1[j+i*(nx+1)])*0.5; //termine galleggiamento
-	double BB=-a-temp+sqrt(i_gr)*(b+c); //approssimo gr>>1 gr/re^2
-	vy1[j + i*(nx+1)]=vy[j + i*(nx+1)]+dt*(BB-(p[j+1+i*(nx+1)]-p[j + i*(nx+1)])*i_dy); //velocita vy al passo n+1
-      }
-
+    //condizioni al contorno vy
+    //parete left right noslip
+    vy1(1:ny+1,1)=-vy1(1:ny+1,2);
+    vy1(1:ny+1,nx+2)=-vy1(1:ny+1,nx+1);
+    //parete top e bottom impermiabilità
+    vy1(1,1:nx+2)=0;
+    vy1(ny+1,1:nx+2)=0;
+    
     break;
   }
 }
