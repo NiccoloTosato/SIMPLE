d81856ac5fc3b3e4bd056b2bb5053459cc42bc83
Niccolo Tosato <niccolo.tosato@yahoo.it>
2023-10-25 11:36:20 +0200

    Almost working

--- simple_gr.cpp
+++ simple_gr.cpp
@@ -14,7 +14,7 @@ private:
   index_type ncols;
   
 public:
-  Grid(const index_type rows,const index_type cols) : data{new T[ rows* cols]},  nrows{ rows }, ncols{ cols} {}
+  Grid(const index_type rows,const index_type cols) : data{new T[ rows* cols]()},  nrows{ rows }, ncols{ cols} {}
 
   T& operator()(index_type row, index_type col) {
     return data[row*ncols + col];
@@ -34,8 +34,8 @@ public:
 
 int main() {
 
-  const unsigned int nx {64};
-  const unsigned int ny {64};
+  const unsigned int nx {256};
+  const unsigned int ny {256};
 
   const double lx {1};
   const double ly {1};
@@ -50,39 +50,43 @@ int main() {
   const double i_dx2 { 1 / dx2 };
   const double i_dy2 { 1 / dy2 };
   
-  const double dt { 0.00005 };
+  const double dt { 0.0001 };
   const double pr { 1 };
   const double i_pr { 1/pr };
-  const double gr { 100 };
+  const double gr { 500 };
   const double i_gr { 1 / gr };
   const double rel { 1.1 };
   
-  const double eps_p { 1.0E-10 };
-  const double eps { 1.0E-10 };
+  const double eps_p { 1.0E-11 };
+  const double eps { 1.0E-11 };
   
 
-  Grid<double,unsigned short int> vx{ny+1,nx};
-  Grid<double,unsigned short int> vx1{ny+1,nx};
+  Grid<double,unsigned short int> vx{ny+2,nx+1};
+  Grid<double,unsigned short int> vx1{ny+2,nx+1};
   
-  Grid<double,unsigned short int> vy{ny,nx+1};
-  Grid<double,unsigned short int> vy1{ny,nx+1};
+  Grid<double,unsigned short int> vy{ny+1,nx+2};
+  Grid<double,unsigned short int> vy1{ny+1,nx+2};
  
-  Grid<double,unsigned short int> p{ny+1,nx+1};
-  Grid<double,unsigned short int> p1{ny+1,nx+1};
-  Grid<double,unsigned short int> p2{ny+1,nx+1};
+  Grid<double,unsigned short int> p{ny+2,nx+2};
+  Grid<double,unsigned short int> p1{ny+2,nx+2};
+  Grid<double,unsigned short int> p2{ny+2,nx+2};
 
   Grid<double,unsigned short int> div{ny,nx};
   
-  Grid<double,unsigned short int> T{ny+1,nx+1};
-  Grid<double,unsigned short int> T1{ny+1,nx+1};
+  Grid<double,unsigned short int> T{ny+2,nx+2};
+  Grid<double,unsigned short int> T1{ny+2,nx+2};
+  
+  std::cout<<"Fine init\n";
   std::size_t iteration=0;
-  while(iteration < 2500) {
-
+  while(iteration < 5500) {
+    std::cout<<"Iter: "<<iteration<<std::endl;
+    #pragma omp parallel for
     //componente x velocità
     for( std::size_t j=1; j < ny; ++j)  //new row 
       for( std::size_t i=1; i < nx-1; ++i) { //next col
 	const double vt=0.5*(vy(j-1,i)+vy(j-1,i+1));
 	const double vb=0.5*(vy(j,i)+vy(j,i+1));
+
 	const double ut=0.5*(vx(j-1,i)+vx(j,i));
 	const double ub=0.5*(vx(j+1,i)+vx(j,i));
 	const double ur=0.5*(vx(j,i)+vx(j,i+1));
@@ -98,6 +102,7 @@ int main() {
 
     
     //componente y velocità
+    #pragma omp parallel for
     for( std::size_t j=1; j < ny - 1; ++j) //new row
       for( std::size_t i=1; i < nx; ++i) { //next col
 	const double ur=0.5*(vx(j,i)+vx(j+1,i));
@@ -117,30 +122,31 @@ int main() {
 
     //condizioni al contorno vx
     //parete left right impermeabilità
-    for(std::size_t i=0; i<ny+1; ++i) {
+    for(std::size_t i=1; i<ny; ++i) {
       vx1(i, 0)=0;
-      vx1(i, nx-1)=0;
+      vx1(i, nx)=0;
     }
     
     //parete top e bottom noslip
-    for(std::size_t i=0; i < nx ; ++i) {
+    for(std::size_t i=0; i < nx+1 ; ++i) {
       vx1(0,i)=2*0-vx1(1,i);
-      vx1(ny,i)=2*0-vx1(ny-1,i);
+      vx1(ny+1,i)=2*0-vx1(ny,i);
     }
     
     //condizioni al contorno vy
     //parete left right noslip
-    for(std::size_t i=0; i<ny;++i) {
+    for(std::size_t i=0; i<ny+1;++i) {
       vy1(i,0)=2*0-vy1(i,1);
-      vy1(i,nx)=2*0-vy1(i,nx-1);
+      vy1(i,nx+1)=2*0-vy1(i,nx);
     }
 
     //parete top e bottom impermiabilità
-    for(std::size_t i=0; i< nx+1;++i) {
-      vy1(1,i)=0;
-      vy1(ny-1,i)=0;
+    for(std::size_t i=1; i< nx;++i) {
+      vy1(0,i)=0;
+      vy1(ny,i)=0;
     }
 
+    #pragma omp parallel for
     //calcolo la divergenza, sarà source di poisson 
     for(std::size_t j=1; j< ny;++j)
       for(std::size_t i=1; i< nx;++i)
@@ -153,10 +159,11 @@ int main() {
 
     //start poisson solver, now
     std::size_t it=0;
-    while(it<1500) {
+    while(it<3000) {
       it++;
       p2.swap(p1);
 
+      #pragma omp parallel for
       //laplace operator
       for (std::size_t j=1;j<ny;++j)
 	for (std::size_t i=1;i<ny;++i) {
@@ -167,37 +174,38 @@ int main() {
       //forziamo le condizioni al contorno
       for(std::size_t i=0;i<ny+1;++i) {
 	p1(i,0)=p1(i,1); //parete sx
-	p1(i,nx)=p1(i,nx-1); //parete dx
+	p1(i,nx+1)=p1(i,nx); //parete dx
       }
       
       for(std::size_t i=0;i<nx+1;++i) {
        	p1(0,i)=p1(1,i); //parete top
-       	p1(ny,i)=p1(ny-1,i); //parete bottom
+       	p1(ny+1,i)=p1(ny,i); //parete bottom
       }
 
       //condizioni contorno agli spigoli
       p1(0,0)=p1(1,1);
-      p1(0,nx)=p1(1,nx-1);
-      p1(ny,nx)=p1(ny-1,nx-1);
-      p1(ny,0)=p1(ny-1,1);
+      p1(0,nx+1)=p1(1,nx);
+      p1(ny+1,nx+1)=p1(ny,nx);
+      p1(ny+1,0)=p1(ny,1);
 
       //TO DO: CHECK CONVERGENCE
       
     }
 
-
+    #pragma omp parallel for
     //correggere velocita x
-    for(std::size_t j=1;j<ny+1;++j)
-      for(std::size_t i=1;i<nx;++i) 
-	vx1(j,i)=vx1(j,i)-(p1(j,i+1)-p1(j,i))*i_dx*dt;
-
-    //correggere velocita y
     for(std::size_t j=1;j<ny;++j)
-      for(std::size_t i=1;i<nx +1 ;++i) 
+      for(std::size_t i=1;i<nx-1;++i) 
+	vx1(j,i)=vx1(j,i)-(p1(j,i+1)-p1(j,i))*i_dx*dt;
+    #pragma omp parallel for
+    //correggere velocita y
+    for(std::size_t j=1;j<ny-1;++j)
+      for(std::size_t i=1;i<nx  ;++i) 
 	vy1(j,i)=vy1(j,i)-(p1(j+1,i)-p1(j,i))*i_dy*dt;
 
-    for(std::size_t j=1;j<ny;++j)
-      for(std::size_t i=1;i<nx;++i) {
+    #pragma omp parallel for
+    for(std::size_t j=1;j<ny+1;++j)
+      for(std::size_t i=1;i<nx+1;++i) {
 	
 	const double u=0.5*(vx1(j,i)+vx1(j,i-1));
 	const double v=0.5*(vy1(j,i)+vy1(j-1,i));
@@ -208,14 +216,15 @@ int main() {
 	T1(j,i)=T(j,i)+dt*(-a-b+c);
       }
 
+
     //condizioni al contorno temperatura
     for (std::size_t i=1;i<ny+1;++i) {
       T1(i,0)=2*1-T1(i,1); //parete sx fredda
-      T1(i,nx)=2*0-T1(i,nx-1); //parete dx sorgente calda
+      T1(i,nx+1)=2*0-T1(i,nx); //parete dx sorgente calda
     }
     for (std::size_t i=1;i<nx+1;++i) {
       T1(0,i)=T1(1,i); //parete top adiabatica
-      T1(ny,i)=T1(ny-1,i); //parete bottom adiabatica
+      T1(ny+1,i)=T1(ny,i); //parete bottom adiabatica
     }
 
     T.swap(T1);
@@ -233,6 +242,7 @@ int main() {
 
   Grid<double,unsigned short int> vxs{ny,nx};
   Grid<double,unsigned short int> vys{ny,nx};
+  #pragma omp parallel for
   for(std::size_t j=0;j<ny;++j)
     for(std::size_t i=0;i<nx;++i) {
       vys(j,i)=0.5*(vy1(j,i+1)+vy1(j+1,i+1));
@@ -244,17 +254,17 @@ int main() {
   //////////////////////////ALTAMENTE SPERIMENTALE E SCHIFOSO///////////////////////////////
   std::ofstream vx_file;
   vx_file.open("vex.bin", std::ios::binary | std::ios::out);
-  vx_file.write((const char*)&vx(0,0), sizeof(double)*nx*ny);
+  vx_file.write((const char*)&vxs(0,0), sizeof(double)*nx*ny);
   vx_file.close();
 
   std::ofstream vy_file;
   vy_file.open("vey.bin", std::ios::binary | std::ios::out);
-  vy_file.write((const char*)&vy(0,0), sizeof(double)*nx*ny);
+  vy_file.write((const char*)&vys(0,0), sizeof(double)*nx*ny);
   vy_file.close();
 
   std::ofstream temp_file;
   temp_file.open("temp.bin", std::ios::binary | std::ios::out);
-  temp_file.write((const char*)&T(0,0), sizeof(double)*nx*ny);
+  temp_file.write((const char*)&T(0,0), sizeof(double)*(nx+2)*(ny+2));
   temp_file.close();
   //////////////////////////################################///////////////////////////////
 
--- visualizza_stream.py
+++ visualizza_stream.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import FortranFile
-nx=64
+nx=128
 lx=1
 dx=lx/nx
 
@@ -12,9 +12,9 @@ datat=np.fromfile("temp.bin","float64",-1,offset=0)
 Y,X=np.mgrid[0:1:dx,0:1:dx]
 vex=np.reshape(datax,(nx,nx),order='C')
 vey=np.reshape(datay,(nx,nx),order='C')
-t=np.reshape(datat,(nx,nx),order='C')
+t=np.reshape(datat,(nx+2,nx+2),order='C')
 plt.figure(1)
 plt.streamplot(X, Y, vex[::1,::1],vey[::1,::1], density=[4.5, 4.5],linewidth=0.9,arrowsize=0.5)
 plt.figure(2)
-plt.contour(X,Y,t,30)
+plt.contour(X,Y,t[1:-1,1:-1],30)
 plt.show()
